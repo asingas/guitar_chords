c4e5e7a49d40ae1d43e85a68acecb5e4b561300d
Arthur Singas <40192855+asingas@users.noreply.github.com>
2018-12-25 18:20:11 -0500

    Update guitar_chords.c

--- guitar_chords.c
+++ guitar_chords.c
@@ -17,11 +17,11 @@
 /* DEFINING MAX SIZE OF A NOTE WHEN REPRESENTED AS A STRING */
 #define MAX_NOTE_SIZE 2
 
-/* GLOBAL VARIABLES */
-char note_requested[MAX_NOTE_SIZE];
-char chord_or_scale;
-char *valid_notes[17] = { "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#" };
-char **scales;
+/* GLOBAL VARIABLES */	
+char note_requested[MAX_NOTE_SIZE];																/* for storing the note value user requests */
+char chord_or_scale;																			/* for taking in user request chords or scale */
+char *valid_notes[17] = { "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#" };	/* list of all valid notes on fretboard */
+int num_scales;																					/* number of scales seen that are relevant to note_requested */
 
 /* Struct of fretboard on with 6 arrays of character pointers, each index representing each note
  * on the fretboard starting from the open string (index: [0]) to the 11th. 12th fret is equal to open string
@@ -37,7 +37,6 @@ struct fretboard {
 };
 
 /* Initialization of fretboard / setting of each index (fret) of string to respective notes */
-/* fret.e_string[0] = E, fret.e_string[11] = D# */
 struct fretboard fret = { { "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#" },
 						  { "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#" },
 						  { "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#" },
@@ -50,51 +49,35 @@ void print_scale(char *note)
 
 }
 
-/* TODO */
 char* parse_available_scales(FILE *in, char *note)
 {	
-	char delimiters[] = ":";
-	char *scale_entry = malloc(101 * sizeof(char));										/* scale_entry is every line of text file */
-	
-	if (fgets(scale_entry, 100, in) == NULL) {
+	char *check;	
+	char **scales = (char **)malloc(100*sizeof(char *));							/* char* to check if current scale is of the correct note  */
+    char delimiters[] = ":";														/* text file elements are separated by ":", must use this token */
+	char *scale_entry_check = (char *)malloc(101 * sizeof(char));					/* scale_entry is every line of text file */
+	char *scale_entry = (char *)malloc(101 * sizeof(char));
+
+	for (int i = 0; i < 3; i++) {
+		*(scales + i) = (char *)malloc(20*sizeof(char *));							/* malloc for 1) note value, 2) name 3) notes of scale */
+	}
+	if (fgets(scale_entry_check, 100, in) == NULL) {								/* setting each line of text file = to scale_entry */
 		return (char *) NULL;
 	}
 
-	//printf("scale entry: %s", scale_entry);
-
-	scales = (char **)malloc(100*sizeof(char*));
-	/* MALLOCING FOR EACH PART OF THE SCALE: 1) note value, 2) name 3) notes of scale */
-	for (int i = 0; i < 3; i++) {
-		*(scales + i) = (char *)malloc(20*sizeof(char *));
-	}
-
-	if (strcmp(strtok(scale_entry, delimiters),note) == 0) {
-	
+	strcpy(scale_entry, scale_entry_check);														
+	check = strtok(scale_entry_check, delimiters);
+	if (strcmp(check, note) == 0) {
 		scales[0] = strtok(scale_entry, delimiters);
 		scales[1] = strtok(NULL, delimiters);
 		scales[2] = strtok(NULL, delimiters);
+		printf("%s", scales[2]);
 	}
-	printf("%s ", scales[0]);
 	return scales[1];
 }
 
 void parse_scale_notes(FILE *in, char *note)
 {
-	/*struct scale_notes scale;
-	char ** notes;
-	char delimiters[s] = ",";
-	if (fgets(traceEntry, 100, in) == NULL)
-		return (char *) NULL;
-
-	notes = (char **)malloc(100*sizeof(char*));
-	for (int i = 0; i < 4; i++){
-		*(notes+i) = (char *)malloc(20*sizeof(char *));
-	}
-	notes[0] = strtok(note, delimiters);
-	notes[1] = strtok(NULL, delimiters);
-	notes[2] = strtok(NULL, delimiters);
-	notes[3] = strtok(NULL, delimiters);
-	*/
+	
 }
 
 void print_chord(char *note)
@@ -102,7 +85,6 @@ void print_chord(char *note)
 
 }
 
-/* TODO */
 void print_fretboard()
 {
 	for (int fret = 0; fret <= 24; fret++) {
@@ -113,29 +95,23 @@ void print_fretboard()
 int get_num_lines_file(char *name)
 {
 	FILE *fp; 
-    int count = 0;  // Line counter (result) 
-    char c;  // To store a character read from file 
+    int count = 0;  																/* Line counter (result) */
+    char c;  										 								/* To store a character read from file  */
   
-    // Open the file 
-    fp = fopen(name, "r"); 
-  
-    // Check if file exists 
-    if (fp == NULL) 
-    { 
+    fp = fopen(name, "r");  	
+    if (fp == NULL) {																/* Check if file exists */ 
         printf("Could not open file %s", name); 
         return 0; 
     } 
   
-    // Extract characters from file and store in character c 
-    for (c = getc(fp); c != EOF; c = getc(fp)) 
-        if (c == '\n') // Increment count if this character is newline 
-            count = count + 1; 
-  
-    // Close the file 
+    for (c = getc(fp); c != EOF; c = getc(fp)) { 									/* Extract characters from file and store in character c */
+        if (c == '\n') {															/* Increment count if this character is newline */
+            count++;
+        }
+  	}
     fclose(fp); 
     return count; 
 } 
-
 
 char* get_note()
 {
@@ -182,28 +158,16 @@ char get_chord_or_scale()
 int main()
 {
 	FILE *to_read = NULL;
-	/* Start by asking for a note on the fretboard */
-	get_note();
-	/* Ask whether user wants chords or scale */
-	get_chord_or_scale();
-	/* Open respective file containing list of scales/chords to parse */
-	if (chord_or_scale == 's') {
+	get_note();																		/* Start by asking for a note on the fretboard */
+	get_chord_or_scale();														    /* Ask whether user wants chords or scale */
+	if (chord_or_scale == 's') {													/* Open respective file containing list of scales/chords to parse */
 		to_read = fopen("scale_list.txt", "r");
 		for (int i = 0; i < get_num_lines_file("scale_list.txt"); i++){
-			parse_available_scales(to_read, note_requested);
+			printf("%s", parse_available_scales(to_read, note_requested));
 		}
 		fclose(to_read);
 	} else if (chord_or_scale == 'c') {
 		to_read = fopen("chord_list.txt", "r");
 	}
 
-	//print_fretboard();
-	//for (int i = 0; i < 12; i++){
-	//	printf("%s ", fret.e_string[i]);
-	
-	//}
-
-
 }
-
-
